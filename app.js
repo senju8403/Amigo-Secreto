102b0b6083012aea3be6f92320c8e4d6b76d4980
jairo <aiglos84@gmail.com>
2025-08-26 18:50:54 +0200

    agrega funcion de agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
+let amigos =[];
+const maxAmigos = 5;
+
+function agregarAmigo(){
+
+  const inputAmigo=document.getElementById("amigo");
+  const listaAmigos=document.getElementById("listaAmigos");
+  const nombre=inputAmigo.value.trim();
+
+
+ 
+   if (nombre === '') {
+                alert('Por favor, ingresa un nombre válido.');
+                return;
+            } 
+if (amigos.length >= maxAmigos) {
+                alert(`Solo puedes agregar hasta ${maxAmigos} amigos.`);
+                return;
+                 }
+if (amigos.includes(nombre)) {
+                alert('Este nombre ya está en la lista.');
+                return;
+                }
+                amigos.push(nombre);
+                inputAmigo.value = '';
+            inputAmigo.focus();
+           
+
+
+}
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
